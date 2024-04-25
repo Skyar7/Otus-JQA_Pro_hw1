622e90bf6cecf5263616696a9be9c77170afa69c
Artem <skyar7@yandex.ru>
2024-04-25 20:53:41 +0700

    tests version 2.0.0

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM maven:3.9.6-eclipse-temurin-8
 
-USER root
-
 RUN mkdir -p /home/ubuntu/ui_tests
 
 WORKDIR /home/ubuntu/ui_tests
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mvn test -Dbrowser.name=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION -Dbase.url=$BASE_URL -Dremote.url=$REMOTE_URL -Ddriver.type=$DRIVER_TYPE
+command="mvn test -Dbrowser.name=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION -Dbase.url=$BASE_URL -Dremote.url=$REMOTE_URL -Ddriver.type=$DRIVER_TYPE"
+
+echo "Run: " $command
+eval $command
+echo "End of running: " $command
